dce788f33e5d7c7bcd22516ab23eb283ce7744f3
wwpkx <wwpkx@163.com>
2014-05-21 11:52:16 +0800

    changed test

--- src/Hello2.java
+++ src/Hello2.java
@@ -5,7 +5,8 @@ public class Hello2 {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("hello world2!");
+        System.out.println("hello world2! and test changed");
     }
 
 }
